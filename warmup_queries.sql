243ea77483dcdaa1914d86ba255f91c1525e3f92
jaforte <forte_j@hotmail.com>
2022-07-09 20:02:49 -0400

    Added Vaccinations, Users, and Articles relations to script.

--- warmup_queries.sql
+++ warmup_queries.sql
@@ -20,6 +20,19 @@ vaccineName varchar(255) not null,
 primary key(vaccineID)
 );
 
+CREATE TABLE Vaccinations (
+countryID int not null,
+vaccineID int not null,
+vaccinated int,
+foreign key (countryID)
+	references Countries(countryID)
+    on delete cascade,
+foreign key (vaccineID)
+	references Vaccines(vaccineID)
+    on delete cascade,
+primary key(countryID, vaccineID)
+);
+
 CREATE TABLE Cases (
 caseID int not null auto_increment,
 countryID int not null,
@@ -33,4 +46,22 @@ foreign key (vaccineID)
 	references Vaccines(vaccineID)
     on delete cascade,
 primary key(caseID)
+);
+
+CREATE TABLE Users (
+userID int not null auto_increment,
+firstName varchar(255),
+lastName varchar(255),
+userType enum('administrator', 'researcher', 'delegate', 'regular'),
+primary key(userID)
+);
+
+CREATE TABLE Articles (
+articleID int not null auto_increment,
+author varchar(255) not null,
+majorTopic varchar(255) not null,
+minorTopic varchar(255),
+summary varchar(255),
+publication date not null,
+primary key(articleID)
 );
