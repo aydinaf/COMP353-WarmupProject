746d53726932604b228192407396012fb2f7fa63
jaforte <forte_j@hotmail.com>
2022-07-09 22:46:47 -0400

    Added Researchers and Orginizations tables. Modified schema of existing tables.

--- warmup_queries.sql
+++ warmup_queries.sql
@@ -7,8 +7,13 @@ countryID int not null auto_increment,
 countryName varchar(255) not null,
 regionName varchar(255) not null,
 population int not null,
+govAgency int not null,
 foreign key (regionName)
 	references Regions(regionName)
+    on delete cascade
+    on update cascade,
+foreign key (govAgency)
+	references Orginizations(orgID)
     on delete cascade,
 primary key(countryID)
 );
@@ -49,18 +54,46 @@ primary key(caseID)
 
 CREATE TABLE Users (
 userID int not null auto_increment,
+firstName varchar(255) not null,
+lastName varchar(255) not null,
+citizenship int not null,
+email varchar(255),
+phone varchar(255),
+userType enum('administrator', 'researcher', 'delegate', 'regular') default 'regular',
+foreign key(citizenship)
+	references Countries(countryID)
+    on delete cascade,
+primary key(userID)
+);
+
+CREATE TABLE Researchers (
+researcherID int not null auto_increment,
 firstName varchar(255),
 lastName varchar(255),
-userType enum('administrator', 'researcher', 'delegate', 'regular'),
-primary key(userID)
+primary key(researcherID)
 );
 
 CREATE TABLE Articles (
 articleID int not null auto_increment,
-author varchar(255) not null,
+researcherID int,
+orgID int,
 majorTopic varchar(255) not null,
 minorTopic varchar(255),
-summary varchar(255),
+summary varchar(100),
 publication date not null,
+foreign key (researcherID)
+	references Researchers(researcherID)
+    on delete cascade,
+foreign key (orgID)
+	references Orginizations(orgID)
+    on delete cascade,    
 primary key(articleID)
 );
+
+CREATE TABLE Orginizations (
+orgID int not null auto_increment,
+orgName varchar(255) not null,
+orgType enum('company', 'research center', 'government agency') default 'government agency',
+primary key(orgID)
+);
+
